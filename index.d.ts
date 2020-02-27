aa769c4d35b1d53e82211dedc3e76901a52a4e2f
pabloABinaria <61514042+pabloABinaria@users.noreply.github.com>
2020-02-27 13:25:24 +0100

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export { ToastAb } from './components/toast/toast';
+export { ToastAbCmp } from './components/toast/toast-component';
+export { ToastAbController } from './components/toast/toast-controller';
+export { ToastAbOptions } from './components/toast/toast-options';
+
